424d8c472260845c90216566f2f0bc3bdee2413d
werenyckyj.vo.2019 <werenyckyj.vo.2019@UC313PC09>
2022-04-11 15:08:01 +0200

    Přidejte soubory projektu.

---
+++ pushpop/pushpop/Page1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pushpop
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void butD_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+    }
+}
